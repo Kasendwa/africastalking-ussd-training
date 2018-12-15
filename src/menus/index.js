7d49440976a8416e560b4109926dadf04bd32e03
Ronald Kasendwa <rkasendwa@outlook.com>
2018-12-15 12:41:32 +0300

    Finished start state run method

--- src/menus/index.js
+++ src/menus/index.js
@@ -17,7 +17,7 @@ export default () => {
 			});
 
 			const registerInstructions = `Welcome to mSACCO
-            \nEnter your first name to register:`;
+            \nEnter your first name tos register:`;
 
 			if (typeof users !== 'undefined') {
 				const user = _.find(
