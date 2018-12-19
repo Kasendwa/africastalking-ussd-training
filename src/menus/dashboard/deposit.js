25dc3a3e6cb9633033d945f9d497523a7c20b0d3
Ronald Kasendwa <rkasendwa@outlook.com>
2018-12-19 17:17:52 +0300

    More work on landing decision block

--- src/menus/dashboard/deposit.js
+++ src/menus/dashboard/deposit.js
@@ -14,12 +14,11 @@ export default menu => {
 			menu.con(
 				_.includes(['1', '0'], `${val}`)
 					? `Deposit Money \nEnter the amount to deposit:`
-					: `Invalid amount provided. Enter an amount between UGX 500 and 3,000,000. \nTry again.`
+					: `Invalid amount provided. \nTry again.`
 			);
 		},
 		next: {
-			'*\\([5-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|[1-8][0-9]{4}|9[0-8][0-9]{3}|99[0-8][0-9]{2}|999[0-8][0-9]|9999[0-9]|[1-8][0-9]{5}|9[0-8][0-9]{4}|99[0-8][0-9]{3}|999[0-8][0-9]{2}|9999[0-8][0-9]|99999[0-9]|[12][0-9]{6}|3000000)':
-				'dashboard.deposit.instructions'
+			'*\\d+': 'dashboard.deposit.instructions'
 		},
 		defaultNext: 'dashboard.deposit'
 	});
@@ -30,6 +29,8 @@ export default menu => {
 				val,
 				args: { phoneNumber }
 			} = menu;
+
+			val = parseFloat(val);
 
 			menu.con(`You have successfully deposited UGX ${val}. \n0. Back`);
 		},
